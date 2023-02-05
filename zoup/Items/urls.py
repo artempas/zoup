87626e9a6dd46c6ca063a5d530fb5cf50d25dbd1
Artempas <Artempas@github.com>
2023-02-05 12:18:18 +0300

    url paths fixed

--- zoup/Items/urls.py
+++ zoup/Items/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path("", views.ItemList.as_view(), name="index"),
     path("logout", LogoutView.as_view(next_page="login"), name="logout"),
-    path("login", views.LoginUser.as_view(), name="login"),
+    path("login/", views.LoginUser.as_view(), name="login"),
     path("register", views.RegisterUser.as_view(), name="register"),
     path("check_item", views.RegisterUser.as_view()),
     path("profile", views.ProfileView.as_view(), name="profile"),
